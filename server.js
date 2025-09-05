fecee50dc63a107aed1cdfe9855dd873eeedd904
Selu <seludemisse12@gmail.com>
2025-09-05 06:42:41 -0700

    Add exams and results feature, update authGuard, remove course route

--- server.js
+++ server.js
@@ -1,28 +1,31 @@
-// server.js
 import express from "express";
-import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import cors from "cors";
-
+import { PrismaClient } from "@prisma/client";
 
 // Routes
 import authRoutes from "./src/routes/auth.js";
 import assignmentRoutes from "./src/routes/assignment.js";
 import profileRoutes from "./src/routes/profile.js";
+import examRoutes from "./src/routes/exam.js";
 import { authGuard } from "./src/middleware/authGuard.js";
 
-
 dotenv.config();
-
 const app = express();
+const prisma = new PrismaClient();
 
-//
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:3000', // frontend URL
-  credentials: true,
+  origin: "http://localhost:3000",
 }));
 
+// attach prisma to req
+app.use((req, res, next) => {
+  req.prisma = prisma;
+  next();
+});
+
+// Get logged-in user info
 app.get("/api/me", authGuard, (req, res) => {
   res.json({ id: req.user.userId, role: req.user.role });
 });
@@ -31,12 +34,11 @@ app.get("/api/me", authGuard, (req, res) => {
 app.use("/auth", authRoutes);
 app.use("/api/assignments", assignmentRoutes);
 app.use("/api/profile", profileRoutes);
-
+app.use("/api/exams", examRoutes);
 
 // default route
 app.get("/", (req, res) => {
-  res.send("LMS backend is running ");
+  res.send("LMS backend is running");
 });
 
-// EXPORT app for index.js
 export default app;
